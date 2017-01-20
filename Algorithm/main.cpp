03ce2fc27e0bb8d8ed3ead126288973d1edad643
hayashifreya0 <hayashifreya0@gmail.com>
2017-01-20 09:22:24 -0300

    Initial Commit

---
+++ Algorithm/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Algorithm
+//
+//  Created by Freya Hayashi on 1/20/17.
+//  Copyright (c) 2017 Freya Hayashi. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
